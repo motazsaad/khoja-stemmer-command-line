4465127cfef6205afecf287d320fe134a41e4f35
Motaz Saad <motaz.saad@gmail.com>
2015-04-29 18:09:11 +0300

    fix readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # khoja Algorithm (a command-line version)
 
-A command line version of Koja Stemmer (An Arabic rooting algorithm)
+A command line version of Koja algorithm (An Arabic rooting algorithm). The algorithm reduces Arabic words to their roots.
 
 Version 1.0
 
@@ -9,7 +9,7 @@ Author: Motaz Saad (motaz dot saad at gmail dot com)
 
 This software is a modification of Khoja algorithm for Arabic rooting. The original implementation is available at http://zeus.cs.pacificu.edu/shereen/ArabicStemmerCode.zip
 
-Khoja algorithm reduces words to their roots. The algorithm is described in:
+The algorithm is described in:
 
 - Khoja S., Garside R., "Stemming Arabic text", Computer Science Department, Lancaster University, Lancaster, UK, 1999.
 
--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 /*
 
+# khoja Algorithm (a command-line version)
+
+A command line version of Koja algorithm (An Arabic rooting algorithm). The algorithm reduces Arabic words to their roots.
+
 Version 1.0
 
 Author: Motaz Saad (motaz dot saad at gmail dot com)
@@ -7,14 +11,11 @@ Author: Motaz Saad (motaz dot saad at gmail dot com)
 
 This software is a modification of Khoja algorithm for Arabic rooting. The original implementation is available at http://zeus.cs.pacificu.edu/shereen/ArabicStemmerCode.zip
 
-Khoja algorithm reduces words to their roots. The algorithm is described in:
+The algorithm is described in:
 
-Khoja S., Garside R., "Stemming Arabic text", Computer Science Department, Lancaster University, Lancaster, UK, 1999.
+- Khoja S., Garside R., "Stemming Arabic text", Computer Science Department, Lancaster University, Lancaster, UK, 1999.
 
-http://zeus.cs.pacificu.edu/shereen/research.htm#stemming
-
- 
-
+- http://zeus.cs.pacificu.edu/shereen/research.htm#stemming
 
 */
 
